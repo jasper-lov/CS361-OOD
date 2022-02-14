fa6a2ee05a54276881938a80cf24a17dce2ef770
mqwen24 <mqwen24@colby.edu>
2022-02-13 21:11:29 -0500

    I created 3 new files: fxmlExample.java fxml_example.fxml fxmlExampleController.java

--- proj2CerratoLoverudeWen/src/proj1CohenCorrellDimitrovLoverude/FXMLExample.java
+++ proj2CerratoLoverudeWen/src/proj1CohenCorrellDimitrovLoverude/FXMLExample.java
@@ -1,8 +1,10 @@
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
 
-public class FXMLExample {
+public class FXMLExample extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
